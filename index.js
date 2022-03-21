54847d39f425597b78a19a6c0cfcbcbc9642e960
TomKrok1 <91782249+trak2022z@users.noreply.github.com>
2022-03-21 14:01:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+"use strict";
+(function() {
+
+  // motivate why we wait for the load to be complete
+  goofy1();
+  goofy2();
+
+   //window.addEventListener("load", init);
+
+  function init() {
+    goofy1();
+    goofy2();
+  }
+
+  function goofy1() {
+    let images = document.querySelectorAll("img");
+    for (let i = 0; i < images.length; i++) {
+      images[i].src = "https://media.giphy.com/media/jaqvaWqpKfImQ/giphy.gif";
+      images[i].alt = "three cute puppers!"
+    }
+  }
+
+  function goofy2() {
+    let pTags = document.querySelectorAll("p");
+    for(let i = 0; i < pTags.length; i++) {
+      pTags[i].textContent = pTags[i].textContent.toUpperCase();
+    }
+  }
+
+})();
